2b4441c0629ada153b220c859dd44d9a9b72655b
Kelly Zhou <kellyyzhou@gmail.com>
2017-11-22 22:45:36 -0500

    Create function and test how to duplicate circles

--- js/custom.js
+++ js/custom.js
@@ -3,8 +3,15 @@ if (canvas.getContext) {
 	var context = canvas.getContext('2d');
 }
 
+function drawCircle(x, y, radius){
+	context.beginPath();
+	context.arc(x, y, radius, 0, Math.PI * 2, true);
+	context.fillStyle = 'blue';
+	context.fill();
+	context.closePath();
+}
+
+for (var i=0; i<1; i++){
+	drawCircle(100, 100, 50);
+}
 // context.fillRect(0, 0, 300, 150);
-context.beginPath();
-context.arc(100, 100, 75, 0, Math.PI * 2, true);
-context.fillStyle = 'blue';
-context.fill();
