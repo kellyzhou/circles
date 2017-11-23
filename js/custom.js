cc166ee9418954dee1586dff2f941db5c1408062
Kelly Zhou <kellyyzhou@gmail.com>
2017-11-22 22:24:33 -0500

    Draw a circle

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+var canvas = document.getElementById('canvas');
+if (canvas.getContext) {
+	var context = canvas.getContext('2d');
+}
+
+// context.fillRect(0, 0, 300, 150);
+context.beginPath();
+context.arc(100, 100, 75, 0, Math.PI * 2, true);
+context.fillStyle = 'blue';
+context.fill();
