62070e117b52de39121ce812f17e635b0337ea0f
Kelly Zhou <kellyyzhou@gmail.com>
2017-11-23 20:54:47 -0800

    Add color to circle function

--- js/compiled/scripts.js
+++ js/compiled/scripts.js
@@ -3,15 +3,18 @@ if (canvas.getContext) {
 	var context = canvas.getContext('2d');
 }
 
-function drawCircle(x, y, radius){
+function drawCircle(x, y, radius, color){
 	context.beginPath();
 	context.arc(x, y, radius, 0, Math.PI * 2, true);
-	context.fillStyle = 'blue';
+	context.fillStyle = color;
 	context.fill();
 	context.closePath();
 }
 
+	drawCircle(100, 100, 50, 'pink');
+	drawCircle(200, 200, 50, 'blue');
+
 for (var i=0; i<1; i++){
-	drawCircle(100, 100, 50);
+	// drawCircle(100, 100, 50, 'pink');
 }
 // context.fillRect(0, 0, 300, 150);
